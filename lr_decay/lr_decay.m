07210137016f1a572fb141418255ad0c701751ae
Jaehoon Shim <71545160+Jaehoon9201@users.noreply.github.com>
2021-10-05 11:11:44 +0900

    Update lr_decay.m

--- lr_decay/lr_decay.m
+++ lr_decay/lr_decay.m
@@ -1,7 +1,7 @@
 close all; clc; clear;
 
-lr_init = 0.004;
-epoch_num = 1200;
+lr_init = 0.0035;
+epoch_num = 1400;
 lr = zeros(epoch_num, 1);
 
 for i = 1:1:4
@@ -11,11 +11,11 @@ for i = 1:1:4
     end
 
 %     plot(1:1:epoch, lr,'-o','MarkerSize',2);
-    plot(1:1:epoch, lr);
-    
+
     colors = get(groot,'defaultAxesColorOrder');
     hold on; grid on; box on;
     plot(1:1:epoch, lr,  'Color' , colors(i,:) ,'LineStyle', '-', 'LineWidth', 1);
-    xlabel('Epoch'); ylabel('Learning Rate');
-    legend('lr_{decay} = 0.098', 'lr_{decay} = 0.096', 'lr_{decay} = 0.094', 'lr_{decay} = 0.092')
 end
+
+xlabel('Epoch'); ylabel('Learning Rate');
+legend('lr_{decay} = 0.098', 'lr_{decay} = 0.096', 'lr_{decay} = 0.094', 'lr_{decay} = 0.092')
